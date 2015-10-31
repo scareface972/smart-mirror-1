d0cfef8d3fcadc87c3c87c6090cf81508f30233a
Steven Vo <findme@stevenvo.info>
2015-10-30 23:37:52 -0700

    modify commands

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
-var FORCAST_API_KEY = "{YOUR_KEY_HERE}";
+var FORCAST_API_KEY = ""; //"b36d1e2bc13b77734fc81cb731159619";
 var HUE_BASE = "http://{IP_ADDRESS_OF_BRIDGE}/api/{Username}/";
--- js/controller.js
+++ js/controller.js
@@ -6,7 +6,7 @@
       $scope.listening = false;
       $scope.debug = false;
       $scope.complement = "Hi, sexy!"
-      $scope.focus = "default";
+      $scope.focus = "Say something, or say 'menu'";
       $scope.user = {};
 
       $scope.colors=["#6ed3cf", "#9068be", "#e1e8f0", "#e62739"];
@@ -37,43 +37,57 @@
          })
 
          //Initiate Hue communication
-         HueService.init();
+         // HueService.init();
 
          var defaultView = function() {
             console.debug("Ok, going to default view...");
-            $scope.focus = "default";
+            $scope.focus = "Say something, or say 'menu'";
          }
 
-         // List commands
-         AnnyangService.addCommand('What can I say', function() {
+         // Show the menu
+         AnnyangService.addCommand('Menu', function() {
             console.debug("Here is a list of commands...");
             console.log(AnnyangService.commands);
             $scope.focus = "commands";
          });
 
+         
+         
          // Go back to default view
-         AnnyangService.addCommand('Go home', defaultView);
+         AnnyangService.addCommand('(Go) home', defaultView);
+         AnnyangService.addCommand('(Go) back', defaultView);
+         
+         
 
          // Hide everything and "sleep"
-         AnnyangService.addCommand('Go to sleep', function() {
+         AnnyangService.addCommand('(Go to) sleep', function() {
             console.debug("Ok, going to sleep...");
             $scope.focus = "sleep";
          });
+         
+
 
          // Go back to default view
          AnnyangService.addCommand('Wake up', defaultView);
+         AnnyangService.addCommand('Mirror', defaultView);
+         
+         
 
-         // Hide everything and "sleep"
-         AnnyangService.addCommand('Show debug information', function() {
-            console.debug("Boop Boop. Showing debug info...");
-            $scope.debug = true;
+         // For debugging
+         AnnyangService.addCommand('Debugging :state', function(state) {
+            console.debug("Boop Boop. Showing debug info: " + state + "...");
+            $scope.debug = state == "on" ? true : false;
          });
+         
+         
 
-         // Hide everything and "sleep"
+         // Show map
          AnnyangService.addCommand('Show map', function() {
             console.debug("Going on an adventure?");
             $scope.focus = "map";
          });
+         
+         
 
          // Hide everything and "sleep"
          AnnyangService.addCommand('Show (me a) map of *location', function(location) {
@@ -135,90 +149,90 @@
          });
 
          // Turn lights off
-         AnnyangService.addCommand('(turn) (the) :state (the) light(s) *action', function(state, action) {
-            HueService.performUpdate(state + " " + action);
-         });
+         // AnnyangService.addCommand('(turn) (the) :state (the) light(s) *action', function(state, action) {
+            //             HueService.performUpdate(state + " " + action);
+            //          });
 
-         // Fallback for all commands
-         AnnyangService.addCommand('*allSpeach', function(allSpeech) {
-            console.debug(allSpeech);
-            _this.addResult(allSpeech);
-         });
+            // Fallback for all commands
+            AnnyangService.addCommand('*allSpeach', function(allSpeech) {
+               console.debug(allSpeech);
+               _this.addResult(allSpeech);
+            });
             
-         //Track when the Annyang is listening to us
-         AnnyangService.start(function(listening){
-            $scope.listening = listening;
-         });
-      };
+            //Track when the Annyang is listening to us
+            AnnyangService.start(function(listening){
+               $scope.listening = listening;
+            });
+         };
         
-      _this.addResult = function(result) {
-         _this.results.push({
-            content: result,
-            date: new Date()
-         });
-      };
+         _this.addResult = function(result) {
+            _this.results.push({
+               content: result,
+               date: new Date()
+            });
+         };
         
-      _this.clearResults = function() {
-         _this.results = [];
-      };
+         _this.clearResults = function() {
+            _this.results = [];
+         };
 
-      _this.init();
+         _this.init();
+      }
+
+      angular.module('SmartMirror')
+      .controller('MirrorCtrl', MirrorCtrl);
+
+   }(window.angular));
+
+
+   /*
+   '(show me) help':       help,
+   'hello (there)':        hello,
+   'stop listening':       stopListening,
+
+
+
+   Commands:
+   "What Can I Say?": give the user a list of availalbe commands
+
+   TODO:
+   - Set a timer for X
+
+   Both the init() and addCommands() methods receive a commands object.
+
+   annyang understands commands with named variables, splats, and optional words.
+
+   Use named variables for one word arguments in your command.
+   Use splats to capture multi-word text at the end of your command (greedy).
+   Use optional words or phrases to define a part of the command as optional.
+   Examples:
+
+   <script>
+   var commands = {
+   // annyang will capture anything after a splat (*) and pass it to the function.
+   // e.g. saying "Show me Batman and Robin" will call showFlickr('Batman and Robin');
+   'show me *term': showFlickr,
+
+   // A named variable is a one word variable, that can fit anywhere in your command.
+   // e.g. saying "calculate October stats" will call calculateStats('October');
+   'calculate :month stats': calculateStats,
+
+   // By defining a part of the following command as optional, annyang will respond
+   // to both: "say hello to my little friend" as well as "say hello friend"
+   'say hello (to my little) friend': greeting
+   };
+
+   var showFlickr = function(term) {
+   var url = 'http://api.flickr.com/services/rest/?tags='+tag;
+   $.getJSON(url);
    }
 
-   angular.module('SmartMirror')
-   .controller('MirrorCtrl', MirrorCtrl);
+   var calculateStats = function(month) {
+   $('#stats').text('Statistics for '+month);
+   }
 
-}(window.angular));
-
-
-/*
-'(show me) help':       help,
-'hello (there)':        hello,
-'stop listening':       stopListening,
-
-
-
-Commands:
-"What Can I Say?": give the user a list of availalbe commands
-
-TODO:
-- Set a timer for X
-
-Both the init() and addCommands() methods receive a commands object.
-
-annyang understands commands with named variables, splats, and optional words.
-
-Use named variables for one word arguments in your command.
-Use splats to capture multi-word text at the end of your command (greedy).
-Use optional words or phrases to define a part of the command as optional.
-Examples:
-
-<script>
-var commands = {
-// annyang will capture anything after a splat (*) and pass it to the function.
-// e.g. saying "Show me Batman and Robin" will call showFlickr('Batman and Robin');
-'show me *term': showFlickr,
-
-// A named variable is a one word variable, that can fit anywhere in your command.
-// e.g. saying "calculate October stats" will call calculateStats('October');
-'calculate :month stats': calculateStats,
-
-// By defining a part of the following command as optional, annyang will respond
-// to both: "say hello to my little friend" as well as "say hello friend"
-'say hello (to my little) friend': greeting
-};
-
-var showFlickr = function(term) {
-var url = 'http://api.flickr.com/services/rest/?tags='+tag;
-$.getJSON(url);
-}
-
-var calculateStats = function(month) {
-$('#stats').text('Statistics for '+month);
-}
-
-var greeting = function() {
-$('#greeting').text('Hello!');
-}
-</script>
-*/ 
+   var greeting = function() {
+   $('#greeting').text('Hello!');
+   }
+   </script>
+   */ 
